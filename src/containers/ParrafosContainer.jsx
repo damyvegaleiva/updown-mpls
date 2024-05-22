4e049dd1131499fbd5cfb1166338059ccfa5df49
damyvegaleiva <damyvegaleiva@gmail.com>
2024-05-22 18:26:04 -0500

    Parrafo text minor fix.

--- src/containers/ParrafosContainer.jsx
+++ src/containers/ParrafosContainer.jsx
@@ -11,7 +11,7 @@ const ParrafosContainer = () => {
     <>
       <Parrafo
         className={"parrafo-one"}
-        heading={"~ Games ~"}
+        heading={"~Games~"}
         svgIcon={<TbDeviceGamepad />}
         text={`Up-Down Minneapolis features more than 50 arcade games from the '80s
           and '90s, pinball machines, four classic skee-ball alleys, bubble
@@ -21,7 +21,7 @@ const ParrafosContainer = () => {
 
       <Parrafo
         className={"parrafo-two"}
-        heading={"~ Drinks ~"}
+        heading={"~Drinks~"}
         svgIcon={<GiDrinkMe />}
         text={`Our drink menu includes an extensive craft beer selection with over 60
         beers on tap and even more bottles and cans. We also feature a full
@@ -30,28 +30,28 @@ const ParrafosContainer = () => {
 
       <Parrafo
         className={"parrafo-three"}
-        heading={"~ Pizzas ~"}
+        heading={"~Pizzas~"}
         svgIcon={<CiPizza style={{ transform: "rotate(-90deg)" }} />}
         text={` Up-Down has house-made pizza by the slice or you can also get a whole one with unique toppings.`}
       />
 
       <Parrafo
         className={"parrafo-four"}
-        heading={"~ Events ~"}
+        heading={"~Events~"}
         svgIcon={<RxCalendar />}
         text={`At Up-Down we like to have fun and we want to make sure that you too, that's why every month we have fun events including: tournaments with prizes, costume contests, ROKKR watch party and more. Make sure to check our events section so you don't miss any of these.`}
       />
 
       <Parrafo
         className={"parrafo-five"}
-        heading={"~ Private Party ~"}
+        heading={"~Private Party~"}
         svgIcon={<TbCalendarCheck />}
         text={`Looking for a place to do your company's employee party? Book it at Up-Down and have a unique experience, reach out to us on our contact section for more info & details.`}
       />
 
       <Parrafo
         className={"parrafo-six"}
-        heading={"~ Pet Friendly ~"}
+        heading={"~Pet Friendly~"}
         svgIcon={<IoPawOutline />}
         text={`We are a pet-friendly place where you can bring your best friend with you. We have a front patio, a back patio and a full size rooftop area for you to bring and have a good time with your pet too.`}
       />
